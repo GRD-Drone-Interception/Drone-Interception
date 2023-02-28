e80847968fd3a9d63896153f6ad631e78bf2e04a
Charlie <charlie.evans1899@gmail.com>
2023-02-28 04:46:12 +0000

    Attachment buttons + slots now highlight when an attachment is selected

--- Assets/Scripts/DroneLoadout/DroneAttachmentOutfitter.cs
+++ Assets/Scripts/DroneLoadout/DroneAttachmentOutfitter.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +10,57 @@ namespace DroneLoadout
     /// </summary>
     public class DroneAttachmentOutfitter : MonoBehaviour
     {
-        //public event Action<DroneAttachmentOutfitter> OnAttachmentSelected;
+        public event Action<DroneAttachmentOutfitter> OnAttachmentSelected;
         
         [SerializeField] private GameObject componentPrefab;
         [SerializeField] private DroneAttachmentSlot droneAttachmentSlot;
         private Button _button;
+        private TMP_Text _text;
+        private ColorBlock _highlightColourBlock;
+        private ColorBlock _unhighlightColourBlock;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _text = GetComponentInChildren<TMP_Text>();
+        }
 
         private void OnEnable() => _button.onClick.AddListener(DecorateAttachmentPoint);
         private void OnDisable() => _button.onClick.RemoveListener(DecorateAttachmentPoint);
-        private void Awake() => _button = GetComponent<Button>();
+
+        private void Start()
+        {
+            _highlightColourBlock = new ColorBlock
+            {
+                normalColor = new Color(0, 1, 0.6f, 0.75f),
+                highlightedColor = _button.colors.highlightedColor,
+                pressedColor = _button.colors.pressedColor,
+                selectedColor = new Color(0, 1, 0.6f, 0.75f),
+                disabledColor = _button.colors.disabledColor,
+                colorMultiplier = 1,
+                fadeDuration = 0
+            };
+            
+            _unhighlightColourBlock = new ColorBlock
+            {
+                normalColor = new Color(0, 0, 0, 0.8f),
+                highlightedColor = _button.colors.highlightedColor,
+                pressedColor = _button.colors.pressedColor,
+                selectedColor = new Color(0, 0, 0, 0.8f),
+                disabledColor = _button.colors.disabledColor,
+                colorMultiplier = 1,
+                fadeDuration = 0
+            };
+        }
+        
+        private void Update()
+        {
+            // TODO: Clean this
+            if (!droneAttachmentSlot.GetAttachmentPoint().HasAttachment)
+            {
+                Unhighlight();
+            }
+        }
 
         private void DecorateAttachmentPoint()
         {
@@ -38,12 +82,25 @@ namespace DroneLoadout
             if (!droneAttachmentSlot.GetAttachmentPoint().HasAttachment)
             {
                 droneAttachmentSlot.GetDrone().Decorate(droneAttachment, attachmentPoint);
-                //OnAttachmentSelected?.Invoke(this);
+                Highlight();
+                OnAttachmentSelected?.Invoke(this);
             }
             else
             {
                 Destroy(droneAttachment.gameObject);
             }
+        }
+
+        private void Highlight()
+        {
+            _button.colors = _highlightColourBlock;
+            _text.color = Color.black;
+        }
+
+        private void Unhighlight()
+        {
+            _button.colors = _unhighlightColourBlock;
+            _text.color = Color.white;
         }
     }
 }
