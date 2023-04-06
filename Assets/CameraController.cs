25c2b434937a0156c3c7ab3fe5c462384d482e54
Charlie <charlie.evans1899@gmail.com>
2023-04-07 00:32:08 +0100

    Camera improvements | See description
    - Added smooth damping to camera movement.
    - Added speed multiplier that increases camera movement speed when shift key is held.
    - Camera can now be moved up and down on Y axis elevation with Q and E keys.
    - Scroll wheel FOV zooming removed. Scroll wheel now moves camera closer or further away.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,27 +4,44 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
     [SerializeField]private float moveSpeed = 20f;
-    [SerializeField]private float zoomSpeed = 100f;
     [SerializeField]private float rotateSpeed = 20f;
+    [SerializeField]private float scrollSpeed = 1000f;
     [SerializeField]private float orbitSpeed = 20f;
+    [SerializeField] private float smoothTime = 0.2f;
     [SerializeField] private float minCameraHeight = 1f;
-    
-    //private bool isRotating = false;
-    private bool _isOrbiting = false;
+    [SerializeField] private float shiftMoveSpeedMultiplier = 2f;
+    private Vector3 _moveVelocity = Vector3.zero;
     private Vector3 _previousMousePosition;
+    private bool _isOrbiting;
 
     private void Update()
     {
         // Move camera
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput);
-        mainCamera.transform.Translate(moveDirection * (moveSpeed * Time.deltaTime));
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized; // Normalize to avoid diagonal movement
+        moveDirection = mainCamera.transform.TransformDirection(moveDirection); // Convert to world space direction
+        moveDirection.y = 0f; // Ignore Y axis
+        float currentMoveSpeed = moveSpeed; 
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) 
+        {
+            currentMoveSpeed *= shiftMoveSpeedMultiplier; 
+        }
+        Vector3 targetPosition = mainCamera.transform.position + moveDirection * (currentMoveSpeed * Time.deltaTime);
+        mainCamera.transform.position = Vector3.SmoothDamp(mainCamera.transform.position, targetPosition, ref _moveVelocity, smoothTime);
 
-        // Zoom camera
-        float zoomInput = Input.GetAxis("Mouse ScrollWheel");
-        mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView - zoomInput * zoomSpeed * Time.deltaTime, 5f, 90f);
+        // Move camera closer/further
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 newScrollPosition = mainCamera.transform.position + mainCamera.transform.forward * (scrollInput * scrollSpeed * Time.deltaTime);
+        mainCamera.transform.position = newScrollPosition;
         
+        // Move camera up/down
+        float upInput = Input.GetKey(KeyCode.Q) ? 1f : 0f;
+        float downInput = Input.GetKey(KeyCode.E) ? 1f : 0f;
+        Vector3 verticalDirection = new Vector3(0f, upInput - downInput, 0f).normalized;
+        Vector3 targetYPosition = mainCamera.transform.position + verticalDirection * (moveSpeed * Time.deltaTime);
+        mainCamera.transform.position = Vector3.SmoothDamp(mainCamera.transform.position, targetYPosition, ref _moveVelocity, smoothTime);
+
         // Rotate camera
         if (Input.GetMouseButton(1))
         {
