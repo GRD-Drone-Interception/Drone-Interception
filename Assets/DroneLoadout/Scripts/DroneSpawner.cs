9d1abc97bc32e725671494c8f2606b2f9507c3c9
Charlie <charlie.evans1899@gmail.com>
2023-04-09 03:31:00 +0100

    Added simple raycast spawner for spawning drones | Ui changes
    - Drones can be spawned in the world on spawnable layers using raycasts
    - Drones can only be spawned within a hardcoded range of each other
    - Changed colour of drone Ui cards

--- Assets/DroneLoadout/Scripts/DroneSpawner.cs
+++ Assets/DroneLoadout/Scripts/DroneSpawner.cs
@@ -1,59 +1,25 @@
 using System;
-using DroneBehaviours.Scripts;
 using DroneLoadout.Decorators;
-using Testing;
 using UnityEngine;
 using UnityEngine.UI;
-using Utility;
 
 namespace DroneLoadout.Scripts
 {
     public class DroneSpawner : MonoBehaviour
     {
-        public event Action<GameObject> OnDroneSpawned;
+        public event Action<DroneSpawner> OnDroneSpawnerSelected;
+        public DroneConfigData DroneConfigData => droneConfigData;
 
         [SerializeField] private DroneConfigData droneConfigData;
         private Button _spawnerButton;
 
         public void Awake() => _spawnerButton = GetComponent<Button>();
-        private void OnEnable() => _spawnerButton.onClick.AddListener(SpawnDrone);
-        private void OnDisable() => _spawnerButton.onClick.RemoveListener(SpawnDrone);
+        private void OnEnable() => _spawnerButton.onClick.AddListener(SelectedDrone);
+        private void OnDisable() => _spawnerButton.onClick.RemoveListener(SelectedDrone);
 
-        public void TestSpawn(string input)
+        private void SelectedDrone()
         {
-            int numOfDrones = int.Parse(input);
-
-            if (numOfDrones == 0 || numOfDrones > 20)
-            {
-                Debug.LogError("You are trying to spawn too few or too many drones at once!");
-                return;
-            }
-
-            for (int i = 0; i < int.Parse(input); i++)
-            {
-                Debug.LogError("Spawn drone");
-                SpawnDrone();
-            }
-        }
-        
-        private void SpawnDrone()
-        {
-            // Load default drone prefab from resources file
-            GameObject dronePrefab = Resources.Load<GameObject>(droneConfigData.PrefabDataPath);
-            GameObject spawnedDrone = Instantiate(dronePrefab);
-            var drone = spawnedDrone.GetComponent<Drone>();
-       
-            // Assemble the drone data
-            if (JsonFileHandler.CheckFileExists(drone.DroneConfigData.DroneName))
-            {
-                DroneLoader.Assemble(drone);
-            }
-
-            spawnedDrone.transform.SetPositionAndRotation(new Vector3(0,0,0), Quaternion.identity);
-            drone.SetTeam(TurnManager.Instance.CurrentTeam); 
-            DroneManager.AddDrone(drone);
-            OnDroneSpawned?.Invoke(spawnedDrone);
-            Debug.Log($"Drone Cost: {drone.DecorableDrone.Cost}");
+            OnDroneSpawnerSelected?.Invoke(this);
         }
     }
 }
