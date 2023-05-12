5381fb404d21d0a92dd900a5993c3a47c1011e32
White-Hot <White-Hot_Games@hotmail.com>
2023-05-12 14:26:07 +0100

    Formation and copter fixes
    lil bits

--- Assets/DroneMovement/Scripts/scr_Drone.cs
+++ Assets/DroneMovement/Scripts/scr_Drone.cs
@@ -6,16 +6,13 @@ public class scr_Drone : MonoBehaviour
     public float rotationSpeed = 2f;
     public float maxVelocityChange = 10f;
     public float arrivalDistance = 1f;
-    public float separationDistance = 2f; // New variable for flocking behavior
     public Transform target;
     private Rigidbody rb;
-    private Collider[] nearbyColliders; // New array for detecting nearby objects
-    private scr_Drone[] nearbyDrones; // New array for storing nearby drone scripts
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        transform.rotation = Quaternion.identity;
     }
 
     private void FixedUpdate()
@@ -25,7 +22,6 @@ public class scr_Drone : MonoBehaviour
             // Move towards the target
             Vector3 targetDirection = target.position - transform.position;
             targetDirection.Normalize();
-
 
             float distanceToTarget = Vector3.Distance(transform.position, target.position);
 
@@ -39,7 +35,7 @@ public class scr_Drone : MonoBehaviour
 
                 // Rotate towards the target
                 float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
-                Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, targetDirection);
+                Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
             }
         }
@@ -47,63 +43,13 @@ public class scr_Drone : MonoBehaviour
         {
             target = TargetController.Instance.CurrentTarget.transform;
         }
-
-        // Flocking behavior
-        nearbyColliders = Physics.OverlapSphere(transform.position, separationDistance); // Detect nearby colliders
-        nearbyDrones = new scr_Drone[nearbyColliders.Length]; // Initialize nearby drone scripts array
-
-        for (int i = 0; i < nearbyColliders.Length; i++)
-        {
-            if (nearbyColliders[i].gameObject != gameObject) // If not self
-            {
-                nearbyDrones[i] = nearbyColliders[i].gameObject.GetComponent<scr_Drone>(); // Get nearby drone script
-            }
-        }
-
-        Vector3 flockDirection = Vector3.zero;
-        int flockCount = 0;
-
-        foreach (scr_Drone nearbyDrone in nearbyDrones)
-        {
-            if (nearbyDrone != null)
-            {
-                float distanceToNearbyDrone = Vector3.Distance(transform.position, nearbyDrone.transform.position);
-
-                if (distanceToNearbyDrone < separationDistance)
-                {
-                    Vector3 separationDirection = transform.position - nearbyDrone.transform.position;
-                    flockDirection += separationDirection.normalized;
-                    flockCount++;
-                }
-            }
-        }
-
-        if (flockCount > 0)
-        {
-            flockDirection /= flockCount;
-        }
-
-        // Move with flock direction
-        if (flockDirection != Vector3.zero)
-        {
-            Vector3 velocity = rb.velocity;
-            Vector3 desiredVelocity = flockDirection.normalized * speed;
-            Vector3 steering = desiredVelocity - velocity;
-            steering = Vector3.ClampMagnitude(steering, maxVelocityChange);
-            rb.AddForce(steering, ForceMode.VelocityChange);
-
-            // Rotate towards flock direction
-            float angle = Mathf.Atan2(flockDirection.y, flockDirection.x) * Mathf.Rad2Deg - 90f;
-            Quaternion targetRotation = Quaternion.Euler(new Vector3(-90f, 0f, angle));
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
     }
-    
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger entered");
         if (other.CompareTag("Object"))
-        { 
+        {
             // If the object has collided with the target
             Destroy(TargetController.Instance.CurrentTarget); // Destroy the current target
             TargetController.Instance.currentTargetIndex++; // Increment the target index
